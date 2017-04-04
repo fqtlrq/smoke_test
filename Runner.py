768f6a79a740b02d9e69a521e4ea2e01b0e2a4fc
diao <diaoyinlong@kongfz.com>
2017-04-04 18:23:26 +0800

    t

--- Runner.py
+++ Runner.py
@@ -1,22 +1,26 @@
-import unittest
-import time
 import os
 import sys
+import time
+import unittest
+
 from Common import Get
-from TestCase.App import Api
 from Core.HTMLTestRunner import HTMLTestRunner
 from Plugins import Mail, Jenkins
+from TestCase.App import Api
 
 job_name = sys.argv[1]
 # job_name = 'LFT_test_9.27_java_ehome_app-native-http_liuyu8080'
+print('==============================================================================')
 x, y, z = Jenkins.get_job_info(job_name)
 jenkins_ls = [('Job Name', job_name),
               ('Version', x),
               ('Builder', y),
               ('Build Finish Time', z)]
+print('Get jenkins info successfully...')
 
 test_suite = unittest.TestSuite()
 test_suite.addTests([Api('testAppNative')])
+print('Finish to run test case...')
 
 timeStampArr = time.localtime(time.time())
 
@@ -26,8 +30,8 @@ file = folder + time.strftime('%H-%M-%S') + '.html'
 
 with open(file, 'w', encoding='utf-8') as f:
     HTMLTestRunner(stream=f, title='Automation Script Report', description=u'').run(test_suite, jenkins_ls)
-
+print('Generate report successfully...')
 with open(file, 'r') as f:
     content = f.read()
 if Mail.send_mail('Automation Script Report', content):
-    print('邮件发送成功！')
+    print('E-mail is sent to builder...')
